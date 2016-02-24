9cd5ab873ba249fbcc9c2842bc4440e39d2570fc
Edder Salim Rosado Lira <erosado@palaceresorts.com>
2016-02-24 18:20:28 -0500

    Se agregaron las Reglas de negocios para : BRAgencies, BRLeadSources, BRMarkets, BRSalesRooms

---
+++ IM.BusinessRules/BR/BRSalesRooms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.BusinessRules.BR
+{
+  public class BRSalesRooms
+  {
+    /// <summary>
+    /// Obtiene el catalogo de SalesRooms
+    /// </summary>
+    /// <param name="status"> 0- Sin filtro, 1-Activos, 2. Inactivos </param>
+    /// <returns>List<Model.GetSalesRooms></returns>
+    public static List<Model.GetSalesRooms> GetSalesRooms(int status)
+    {
+      try
+      {
+        using (var model = new IM.Model.IMEntities())
+        {
+          return model.USP_OR_GetSalesRooms(Convert.ToByte(status)).ToList();
+        }
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+
+    }
+    /// <summary>
+    /// Obtiene la lista de SalesRooms
+    /// </summary>
+    /// <param name="user">Usuario o default('ALL')</param>
+    /// <param name="regions">Region o default('ALL')</param>
+    /// <returns></returns>
+    public static List<Model.GetSalesRoomsByUser> GetSalesRoomsByUser(string user, string regions)
+    {
+      try
+      {
+        using (var model = new IM.Model.IMEntities())
+        {
+          return model.USP_OR_GetSalesRoomsByUser(user,regions).ToList();
+        }
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+
+    }
+
+  }
+}
