4c072eda54b0663d18af665cd67d986fffeffa08
Luis Yazmani Chairez Aguilera <lchairez@palaceresorts.com>
2016-02-25 11:05:53 -0500

    Se corrigue Ventana Launcher, para que se ajuste al tamaño de la ventana.

--- IM.Base/Forms/frmInvitationBase.xaml.cs
+++ IM.Base/Forms/frmInvitationBase.xaml.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace IM.Base.Forms
 {
@@ -23,5 +16,6 @@ namespace IM.Base.Forms
         {
             InitializeComponent();
         }
+
     }
 }
