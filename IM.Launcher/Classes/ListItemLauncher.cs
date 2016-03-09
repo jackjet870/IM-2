035b928bff1464ac10ef132575a16c9ca6459ff2
Luis Yazmani Chairez Aguilera <lchairez@palaceresorts.com>
2016-03-09 13:54:59 -0500

    Se sube nuevo Launcher

---
+++ IM.Launcher/Classes/ListItemLauncher.cs
@@ -0,0 +1,21 @@
+﻿using IM.Launcher.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace IM.Launcher.Classes
+{
+  public class ListItemLauncher
+  {
+    public int Orden { get; set; }
+
+    public string Descripcion  { get; set; }
+
+    public string UriImage { get; set; }
+
+    public BitmapImage Image { get; set; }
+  }
+}
