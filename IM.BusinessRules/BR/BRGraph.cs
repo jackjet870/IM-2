e7bc79728470cabf07949edb016f0e6a13b3ffdf
Asuncion Alcocer Alcocer <aalcocer@palaceresorts.com>
2016-03-09 14:07:24 -0500

    IM.BusinessRules 1.-Se agrega la clase BRGraph con el metodo GetGraphProductionByPR 2. se agrega el metodo GetLeadSources en BRLeadSources

---
+++ IM.BusinessRules/BR/BRGraph.cs
@@ -0,0 +1,32 @@
+﻿using IM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IM.BusinessRules.BR
+{
+  public class BRGraph
+  {
+    #region GetGraphProductionByPR
+
+    /// <summary>
+    /// Devuelve los datos para la grafica de produccion de PR
+    /// </summary>
+    /// <param name="dateFrom">Fecha desde</param>
+    /// <param name="dateTo">Fecha hasta</param>
+    /// <param name="leadSource">Clave de Lead Source</param>
+    /// <returns>List<GraphProductionByPRData></returns>
+    /// <history>
+    /// [aalcocer] 07/03/2016 Created
+    /// </history>
+    public static List<GraphProductionByPRData> GetGraphProductionByPR(DateTime dateFrom, DateTime dateTo, string leadSource)
+    {
+      using (var dbContext = new IMEntities())
+      {
+        return dbContext.USP_OR_GraphProductionByPR(dateFrom, dateTo, leadSource).OrderBy(gp => gp.PR).ToList();
+      }
+    }
+
+    #endregion GetGraphProductionByPR
+  }
+}
--- IM.BusinessRules/BR/BRLeadSources.cs
+++ IM.BusinessRules/BR/BRLeadSources.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using IM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using IM.Model;
 
 namespace IM.BusinessRules.BR
 {
@@ -23,6 +23,28 @@ namespace IM.BusinessRules.BR
       }
     }
 
-    #endregion
+    #endregion GetLeadSourcesByUser
+
+    #region GetLeadSources
+
+    /// <summary>
+    /// Obtiene catalogo de Lead Sources .
+    /// </summary>
+    /// <param name="status"> 0- Sin filtro, 1-Activos, 2. Inactivos </param>
+    /// <returns>List<LeadSource></returns>
+    /// <history>
+    /// [aalcocer] 09/03/2016 Created
+    /// </history>
+    public static List<LeadSource> GetLeadSources(int status = 0)
+    {
+      using (var dbContext = new IMEntities())
+      {
+        return dbContext.LeadSources.
+          Where(ls => status.Equals(1) ? ls.lsA : status.Equals(2) ? !ls.lsA : true).
+          OrderBy(ls => ls.lsN).ToList();
+      }
+    }
+
+    #endregion GetLeadSources
   }
 }
