66dcead64556d2891ebc79f852715786a0a8cda8
Edder Salim Rosado Lira <erosado@palaceresorts.com>
2016-03-12 10:34:50 -0500

    Se agrego la clase GridHelper.cs y el metodo estatico  GetDataTableFromObjectList convierte una lista tipada a datatable

---
+++ IM.Base/Helpers/GridHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.ComponentModel;
+
+namespace IM.Base.Helpers
+{
+  public class GridHelper
+  {
+    /// <summary>
+    /// Convierte una lista tipada a un dataTable
+    /// </summary>
+    /// <typeparam name="T">Tipo de objeto de la lista</typeparam>
+    /// <param name="lst">Lista tipada</param>
+    /// <returns>DataTable</returns>
+    /// <history>
+    /// [erosado] 12/03/2016  Created.
+    /// </history>
+    public static DataTable GetDataTableFromObjectList<T>(List<T> lst)
+    {
+      PropertyDescriptorCollection properties =
+           TypeDescriptor.GetProperties(typeof(T));
+      DataTable table = new DataTable();
+      foreach (PropertyDescriptor prop in properties)
+        table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+      foreach (T item in lst)
+      {
+        DataRow row = table.NewRow();
+        foreach (PropertyDescriptor prop in properties)
+          row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+        table.Rows.Add(row);
+      }
+      return table;
+    }
+  }
+}
