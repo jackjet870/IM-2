7b10bb4f9c30800171134ced6f33d420e5cbcada
Edder Salim Rosado Lira <erosado@palaceresorts.com>
2016-03-24 12:07:53 -0500

    se cambio el formato de DateRange para que quede igual a la de origos

--- IM.Base/Helpers/DateHelper.cs
+++ IM.Base/Helpers/DateHelper.cs
@@ -22,24 +22,24 @@ namespace IM.Base.Helpers
     {
       string dateRange = string.Empty;
       //Comparar si es la misma fecha
-      if (dateFrom.ToShortDateString() == dateTo.ToShortDateString())
+      if (dateFrom.Date == dateTo.Date)
       {
-        dateRange = string.Concat("dd-MMMM-yyyy", dateFrom.ToShortDateString());
+        dateRange = string.Format("{0:MMMM d, yyyy}", dateFrom);
       }
       // Si son diferentes años
       else if (dateFrom.Year != dateTo.Year)
       {
-        dateRange = string.Concat(dateFrom.ToString("dd-MMMM-yyyy"), " - ", dateTo.ToString("dd-MMMM-yyyy"));
+        dateRange = string.Format("{0:MMMM d, yyyy} - {1:MMMM d, yyyy}", dateFrom, dateTo);
       }
       //Si son diferentes Meses
       else if (dateFrom.Month != dateTo.Month)
       {
-        dateRange = string.Concat(dateFrom.ToString("dd"), " de ", dateFrom.ToString("MMMM"), " - ", dateTo.ToString("dd"), " de ", dateTo.ToString("MMMM"), " del ", dateTo.ToString("yyyy"));
+        dateRange = string.Format("{0:MMMM d} - {1:MMMM d, yyyy}", dateFrom, dateTo);
       }
       // Mismo mes y mismo año
       else
       {
-        dateRange = string.Concat(dateFrom.ToString("dd"), " - ", dateTo.ToString("dd"), " de ", dateTo.ToString("MMMM"), " del ", dateTo.ToString("yyyy"));
+        dateRange = string.Format("{0:MMMM d} - {1:d, yyyy}", dateFrom, dateTo);
       }
 
       return dateRange;
