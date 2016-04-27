c69cc2d0dd59628c2d0a813f59bc13c5e2168dd8
Edder Salim Rosado Lira <erosado@palaceresorts.com>
2016-04-27 12:20:00 -0500

    Se elimino una columna de Location

--- IM.Model/IMModel.Designer.cs
+++ IM.Model/IMModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'M:\PalaceResorts\Client4.6\Win\Control Model\IntelligenceMarketing\IM.Model\IMModel.edmx'. 
+﻿// T4 code generation is enabled for model 'M:\PalaceResorts\Client4.6\Win\Control Model\IntelligenceMarketing - Copy\IM.Model\IMModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- IM.Model/Location.cs
+++ IM.Model/Location.cs
@@ -35,7 +35,6 @@ namespace IM.Model
         public bool loAnimacion { get; set; }
         public bool loFlyers { get; set; }
         public string lolc { get; set; }
-        public string loBankAffiliation { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<GiftsReceipt> GiftsReceipts { internal get; set; }
