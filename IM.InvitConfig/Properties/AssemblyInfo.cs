49ce84f07492737930590e9bce0aeb22896d394f
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-05-13 12:34:36 -0500

    Configuré los paquetes de los proyectos que le hacían falta mediante el NuGet Manager 1. Entity Framework. Sólo el Model y el Business Rules deben tener como referencia a él 2. EPPlus. Lo agregué como paquete NuGet a los proyectos de Assigment y Processor Outhouse 3. Hardcodet.NotifyIcon.Wpf. Eliminé el paquete del proyecto Transfer 4. EppLib. Eliminé el paquete del proyecto Processor Sales

--- IM.InvitConfig/Properties/AssemblyInfo.cs
+++ IM.InvitConfig/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Intelligence Marketing - InvitConfig")]
+[assembly: AssemblyTitle("Intelligence Marketing - Invitation Configuration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Amazon.com")]
-[assembly: AssemblyProduct("Intelligence Marketing - InvitConfig")]
-[assembly: AssemblyCopyright("Copyright © Amazon.com 2016")]
+[assembly: AssemblyCompany("Palace Resorts")]
+[assembly: AssemblyProduct("Intelligence Marketing - Invitation Configuration")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
