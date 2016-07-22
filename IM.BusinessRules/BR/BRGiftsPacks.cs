457bf3bc302c3957702edbf604746a54b24847a2
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-07-21 19:04:29 -0500

    Me faltaron subir al repositorio unas entidades de IM.Model y unos nuevos Business Rules

---
+++ IM.BusinessRules/BR/BRGiftsPacks.cs
@@ -0,0 +1,45 @@
+﻿using IM.Model;
+using IM.Model.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.BusinessRules.BR
+{
+  /// <summary>
+  /// Clase para el manejo de las consultas a la tabla GiftsPacks
+  /// </summary>
+  /// <history>
+  /// [vipacheco] 06/Julio/2016
+  /// </history>
+  public class BRGiftsPacks
+  {
+
+    #region GetGiftsPacks
+    /// <summary>
+    /// Obtiene los paquetes de gifts
+    /// </summary>
+    /// <returns></returns>
+    /// <history>
+    /// [vipacheco] 07/Julio/2016 Created
+    /// </history>
+    public async static Task<List<GiftPackageItem>> GetGiftsPacks()
+    {
+      List<GiftPackageItem> lstResult = new List<GiftPackageItem>();
+      await Task.Run(() =>
+      {
+        using (var dbContext = new IMEntities(ConnectionHelper.ConnectionString()))
+        {
+          lstResult = dbContext.GiftsPackagesItems.ToList();
+        }
+      });
+
+      return lstResult;
+    } 
+    #endregion
+
+
+  }
+}
---
+++ IM.BusinessRules/BRIC/BRMemberSalesman.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IM.Model;
+using IM.Model.Enums;
+using IM.Model.Helpers;
+
+namespace IM.BusinessRules.BRIC
+{
+  /// <summary>
+  /// Clase para el manejor de los Salesman de la base Hotel(Intelligence Contracts)
+  /// </summary>
+  /// <history>
+  /// [jorcanche]  created 08/07/2016
+  /// </history>
+  public class BRMemberSalesman
+  {
+    #region GetMemberSalesmen
+
+    /// <summary>
+    /// Devuelve la lista MemberSalesmen
+    /// </summary>    
+    /// <returns>Lista de tipo MemberSalesmen</returns>
+    /// <history>
+    /// [jorcanche] created 08/07/2016
+    /// </history>
+    public static async Task<List<MemberSalesmen>> GetMemberSalesmen(string application, string job, string code , string roles)
+    {
+      return await Task.Run(() =>
+      {
+        using (var dbContext = new ICEntities(ConnectionHelper.ConnectionString(EnumDatabase.IntelligenceContracts)))
+        {
+          return dbContext.USP_CL_GetMemberSalesmen(application, job, code, roles).ToList();
+        }
+      });
+    }
+
+    #endregion
+
+    #region SaveMemberSalesman
+
+    /// <summary>
+    /// Obtiene el resultado en tipo de  Salesman cuando se guarda est
+    /// </summary>
+    /// <param name="rEcnum"></param>
+    /// <param name="memberSalesmen"></param>
+    /// <param name="user"></param>
+    /// <history>
+    /// [jorcanche]  created 08/07/2016
+    /// </history>        
+    public static async Task<MemberSalesmen> SaveMemberSalesman(decimal? rEcnum, MemberSalesmen memberSalesmen, string user)
+    {
+      return await Task.Run(() =>
+      {
+        using (var dbContext = new ICEntities(ConnectionHelper.ConnectionString(EnumDatabase.IntelligenceContracts)))
+        {
+          return dbContext.USP_CL_SaveMemberSalesman(
+            rEcnum, memberSalesmen.CLMEMOPC_ID, memberSalesmen.APPLICATION, memberSalesmen.CLAOPC_ID, memberSalesmen.OPC1,
+            memberSalesmen.OPC_PCT,memberSalesmen.OPC_PCT2, memberSalesmen.OPC_CPT3, memberSalesmen.OPC_PCT4, memberSalesmen.STATUS,
+            memberSalesmen.ZONA, memberSalesmen.OPC, user)
+            .FirstOrDefault();
+        }
+      });
+    }
+    #endregion
+
+    #region DeleteMemberSalesman
+    /// <summary>
+    /// Elimina MemberSalesmen
+    /// </summary>        
+    /// <history>
+    /// [jorcanche] created 08/07/2016
+    /// </history>
+    public static async Task<int> DeleteMemberSalesman(decimal? cLmemopcId)
+    {
+      return await Task.Run(() =>
+      {
+        using (var dbContext = new ICEntities(ConnectionHelper.ConnectionString(EnumDatabase.IntelligenceContracts)))
+        {
+          return dbContext.USP_CL_DeleteMemberSalesman(cLmemopcId);
+        }
+      });
+    }
+    #endregion
+
+    #region ExistsSalesman
+    /// <summary>
+    /// Indica si existe un Salesman
+    /// </summary>
+    /// <param name="zOna"></param>
+    /// <param name="cOde"></param>
+    /// <history>
+    /// [jorcanche] created 08/07/2016
+    /// </history>
+    public static async Task<bool> ExistsSalesman(string zOna, string cOde)
+    {
+      return await Task.Run(() =>
+      {
+        using (var dbContext = new ICEntities(ConnectionHelper.ConnectionString(EnumDatabase.IntelligenceContracts)))
+        {
+         return  dbContext.USP_CL_ExistsSalesman(zOna, cOde).FirstOrDefault() == 1;         
+        }
+      });
+    } 
+    #endregion
+  }
+}
