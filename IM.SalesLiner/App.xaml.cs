76483348888495ddcca5a027ae88f7514c063f50
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-07-25 17:55:55 -0500

    Publicación a PRODUCCION del módulo Sales by Liner 1. Actualización del módulo Model 2. Formulario de Login. Ajuste para que se vean completos los textos de los checkboxes 3. DateHelper. Corrección de error en el formato de meses incompletos

---
+++ IM.SalesLiner/App.xaml.cs
@@ -0,0 +1,100 @@
+﻿using IM.Base.Forms;
+using IM.Model.Classes;
+using IM.Model.Enums;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace IM.SalesLiner
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    #region Propiedades
+
+    public static UserData User;
+
+    #endregion
+
+    #region Constructores y destructores
+    /// <summary>
+    /// Constructor de la aplicacion
+    /// </summary>
+    /// <history>
+    ///   [erosado]  22/Mar/2016 Created
+    /// </history>
+    public App() : base()
+    {
+      this.Dispatcher.UnhandledException += App_UnhandledException;
+    }
+    #endregion
+
+    #region Metodos
+
+    #region OnStartup
+    /// <summary>
+    /// Inicializa el modulo con el Login y el Splash
+    /// </summary>
+    protected async  override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+      var frmSplash = new frmSplash("Sales by Liner");
+      var frmLogin = new frmLogin(frmSplash, EnumLoginType.SalesRoom, changePassword:true, autoSign:true);
+      await frmLogin.getAllPlaces();
+      frmSplash.Show();
+      frmSplash.ShowLogin(ref frmLogin);
+      if (frmLogin.IsAuthenticated)
+      {
+        EventManager.RegisterClassHandler(typeof(DataGrid), UIElement.MouseLeftButtonUpEvent, new MouseButtonEventHandler(dataGrid_MouseLeftButtonUp));
+        User = frmLogin.UserData;
+        var frmMain = new Forms.frmSalesLiner();
+        frmMain.ShowDialog();
+        frmSplash.Close();
+      }
+    }
+    #endregion
+
+    #region App_UnhandledException
+    /// <summary>
+    /// Despliega los mensajes de error de la aplicacion
+    /// </summary>
+    /// <history>
+    ///  [erosado]  09/Mar/2016 Created
+    /// </history>
+    private void App_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+      e.Handled = true;
+      var frm = new frmError(e.Exception);
+      frm.ShowDialog();
+      if (frm.DialogResult.HasValue && !frm.DialogResult.Value)
+      {
+        Application.Current.Shutdown();
+      }
+    }
+    #endregion
+    
+    #region dataGrid_MouseLeftButtonUp
+    /// <summary>
+    /// Cambia el cmapo de busqueda del grid
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// <history>
+    /// [erosado] created 21/06/2016
+    /// </history>
+    private void dataGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      DataGrid dgr = sender as DataGrid;
+      if (dgr.CurrentColumn != null)
+      {
+        dgr.Resources["SearchField"] = dgr.CurrentColumn.SortMemberPath;
+      }
+    }
+    #endregion
+
+    #endregion
+  }
+}
