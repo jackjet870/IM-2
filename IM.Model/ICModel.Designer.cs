3cb2f36c7639be57dd79ef002bfcc54b94e39302
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-08-01 16:24:45 -0500

    1. Agregué las siguientes tablas al modelo de Entity Framework de IC (Intelligence Contracts):    a. analista_h.ClMEMBER (entidad Member)

--- IM.Model/ICModel.Designer.cs
+++ IM.Model/ICModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'M:\PalaceResorts\Client4.6\Win\Control Model\IntelligenceMarketing\IM.Model\ICModel.edmx'. 
+﻿// T4 code generation is enabled for model 'M:\PalaceResorts\Client4.6\Win\IntelligenceMarketing\IM.Model\ICModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
