cda0fe07cb2db693ca65da073e678a8fd8601688
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-08-12 15:03:05 -0500

    Actualicé las entidades de los procedimientos almacenados: 1. USP_IM_RptStatisticsByFTB 2. USP_IM_RptStatisticsByFTBCategories 3. USP_IM_RptStatisticsByFTBLocations

--- IM.Model/RptStatisticsByFTBLocations.cs
+++ IM.Model/RptStatisticsByFTBLocations.cs
@@ -17,19 +17,18 @@ namespace IM.Model
         public string SalemanName { get; set; }
         public string Locations { get; set; }
         public string PostName { get; set; }
-        public string TeamN { get; set; }
-        public string TeamLeaderN { get; set; }
         public string SalesmanStatus { get; set; }
         public Nullable<decimal> UPS { get; set; }
-        public Nullable<decimal> OWN { get; set; }
         public Nullable<decimal> WithCloser { get; set; }
         public Nullable<decimal> AsCloser { get; set; }
         public Nullable<decimal> Total { get; set; }
         public Nullable<decimal> Sales { get; set; }
         public Nullable<decimal> Exit { get; set; }
-        public Nullable<decimal> SalesTotal { get; set; }
         public Nullable<decimal> Efficiency { get; set; }
         public Nullable<decimal> ClosingFactor { get; set; }
         public Nullable<decimal> SaleAverage { get; set; }
+        public string Team { get; set; }
+        public Nullable<decimal> Own { get; set; }
+        public Nullable<decimal> TotalSales { get; set; }
     }
 }
