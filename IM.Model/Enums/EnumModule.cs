15518875306cabc41f7b3adc674b16070f098afa
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-08-16 12:50:05 -0500

    1. Eliminé el enumerado EnumInvitationType2 2. EnumModule. Agregue temporalmente los valores Animation, Regen y External. Hay que eliminarlos cuando se elimine el formulario de invitacion de Chairez

--- IM.Model/Enums/EnumModule.cs
+++ IM.Model/Enums/EnumModule.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace IM.Model.Enums
 {
+  /// <summary>
+  /// Enumerado de los diferentes modulos de invitacion
+  /// </summary>
+  /// <history>
+  /// [erosado] 10/Ago/2016 Created
+  /// [wtorres] 16/Ago/2016 Modified. Agregue temporalmente los valores Animation, Regen y External
+  /// </history>
   public enum EnumModule
   {
     InHouse,
     OutHouse,
     Host,
+
+    //TODO: Eliminar los valores Animation, Regen y External cuando se elimine el formulario de invitacion de Chairez
+    Animation,
+    Regen,
+    External
   }
 }
