ae5849081da8ba45fa89b142b5ef4a8ee66893d0
Edgar Rene Rodriguez Medina <edgrodriguez@palaceresorts.com>
2016-08-25 15:18:44 -0500

    Modificaciones.
    IM.Base
    	-ObjReports
    		Se modificaron las clases de RptInvitation.
    
    	-Reports
    		Se modificó el reporte RptInvitation.

--- IM.Base/Classes/ObjReports.cs
+++ IM.Base/Classes/ObjReports.cs
@@ -1,4 +1,6 @@
 ﻿using IM.Model;
+using System;
+using System.Reflection;
 
 namespace IM.Base.Classes
 {
@@ -11,12 +13,40 @@ namespace IM.Base.Classes
   /// </history>
  
     #region Invitation
-    public class RptInvitationIM : RptInvitation { }
+    public class RptInvitationIM : RptInvitation {
+    public DateTime InvitT => guInvitT ?? new DateTime(1899, 12, 31);
+    public DateTime InvitD => guInvitD ?? new DateTime(1899, 12, 31);
+    public RptInvitationIM(RptInvitation parent)
+    {
+      foreach (PropertyInfo prop in parent.GetType().GetProperties())
+        GetType().GetProperty(prop.Name).SetValue(this, prop.GetValue(parent, null), null);
+    }
+  }
 
-    public class RptInvitationDepositsIM : RptInvitation_Deposit { }
+    public class RptInvitationDepositsIM : RptInvitation_Deposit {
+    public RptInvitationDepositsIM(RptInvitation_Deposit parent)
+    {
+      foreach (PropertyInfo prop in parent.GetType().GetProperties())
+        GetType().GetProperty(prop.Name).SetValue(this, prop.GetValue(parent, null), null);
+    }
+  }
 
-    public class RptInvitationGiftsIM : RptInvitation_Gift { }
+    public class RptInvitationGiftsIM : RptInvitation_Gift {
+    public int guestID => guID ?? 0;
+    public RptInvitationGiftsIM(RptInvitation_Gift parent)
+    {
+      foreach (PropertyInfo prop in parent.GetType().GetProperties())
+        GetType().GetProperty(prop.Name).SetValue(this, prop.GetValue(parent, null), null);
+    }
+  }
 
-    public class RptInvitationGuestsIM : RptInvitation_Guest { }
+    public class RptInvitationGuestsIM : RptInvitation_Guest {
+    public int guAge => (Age != null) ? Convert.ToInt32(Age) : 0;
+    public RptInvitationGuestsIM(RptInvitation_Guest parent)
+    {
+      foreach (PropertyInfo prop in parent.GetType().GetProperties())
+        GetType().GetProperty(prop.Name).SetValue(this, prop.GetValue(parent, null), null);
+    }
+  }
     #endregion
 }
--- IM.Base/Helpers/RptInvitationHelper.cs
+++ IM.Base/Helpers/RptInvitationHelper.cs
@@ -4,6 +4,8 @@ using IM.BusinessRules.BR;
 using IM.Model.Classes;
 using IM.Model.Enums;
 using IM.Model.Helpers;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 
 namespace IM.Base.Helpers
@@ -29,7 +31,7 @@ namespace IM.Base.Helpers
     {
       //Traemos la informacion del store y la almacenamos en un procedimiento
       InvitationData invitationData = await BRInvitation.RptInvitationData(guest);
-      
+
       //Le damos memoria al reporte de Invitacion
       var rptInvi = new rptInvitation();
 
@@ -44,11 +46,11 @@ namespace IM.Base.Helpers
       *************************************************************************************************************/
 
       //Le agregamos la informacion 
-      rptInvi.SetDataSource(TableHelper.GetDataTableFromList(ObjectHelper.ObjectToList(invitationData.Invitation)));
+      rptInvi.SetDataSource(TableHelper.GetDataTableFromList(ObjectHelper.ObjectToList(new IM.Base.Classes.RptInvitationIM(invitationData.Invitation))));
       //Cargamos los subreportes
-      rptInvi.Subreports["rptInvitationGuests.rpt"].SetDataSource(TableHelper.GetDataTableFromList(invitationData.InvitationGuest));
-      rptInvi.Subreports["rptInvitationDeposits.rpt"].SetDataSource(invitationData.InvitationDeposit);
-      rptInvi.Subreports["rptInvitationGifts.rpt"].SetDataSource(TableHelper.GetDataTableFromList(invitationData.InvitationGift));
+      rptInvi.Subreports["rptInvitationGuests.rpt"].SetDataSource(TableHelper.GetDataTableFromList(invitationData.InvitationGuest?.Select(c => new IM.Base.Classes.RptInvitationGuestsIM(c)).ToList() ?? new List<Classes.RptInvitationGuestsIM>()));
+      rptInvi.Subreports["rptInvitationDeposits.rpt"].SetDataSource(invitationData.InvitationDeposit?.Select(c => new IM.Base.Classes.RptInvitationDepositsIM(c)).ToList() ?? new List<Classes.RptInvitationDepositsIM>());
+      rptInvi.Subreports["rptInvitationGifts.rpt"].SetDataSource(TableHelper.GetDataTableFromList(invitationData.InvitationGift?.Select(c => new IM.Base.Classes.RptInvitationGiftsIM(c)).ToList() ?? new List<Classes.RptInvitationGiftsIM>()));
 
       //Cambiamos el lenguaje de las etiquetas.
       CrystalReportHelper.SetLanguage(rptInvi, invitationData.Invitation.gula);
