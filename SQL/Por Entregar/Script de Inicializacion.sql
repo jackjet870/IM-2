a5d6323eb02643efcf9e8dbe985a7df940dd6405
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-09-09 09:46:57 -0500

    1. Permisos. Agregué el permiso de exportar reportes a Excel 2. Segmentos por Agencia y Segementos por Lead Source. Agregué la llave foránea con el catálogo de categorías de segmentos 3. Formulario Contact. Eliminé un Warning

--- SQL/Por Entregar/Script de Inicializacion.sql
+++ SQL/Por Entregar/Script de Inicializacion.sql
@@ -10,7 +10,29 @@
 use OrigosVCPalace
 
 -- I. CATALOGOS
+--		1. Permisos
+--		2. Permisos de personal
 
 -- =============================================
 --					CATALOGOS
 -- =============================================
+-- Tabla:		Permisos
+-- [wtorres]	07/Sep/2016 Agregue el permiso de exportar reportes a Excel
+-- =============================================
+INSERT INTO Permissions (pmID, pmN, pmA, pmToolTipText)
+SELECT 'RPTEXCEL', 'Report Excel', 1, 'Permite exportar los reportes a Excel'
+GO
+
+-- =============================================
+-- Tabla:		Permisos de personal
+-- [wtorres]	07/Sep/2016 Agregue el permiso de exportar reportes a Excel al personal de Contraloria,
+--							Soporte Tecnico y Programacion
+-- =============================================
+INSERT INTO PersonnelPermissions (pppe, pppm, pppl)
+SELECT peID, 'RPTEXCEL', 1
+FROM Personnel
+-- CONTR	Contraloria
+-- SOPORTE	Soporte Tecnico
+-- PROG		Programacion
+WHERE pede IN ('CONTR', 'SOPORTE', 'PROG') AND peA = 1
+GO
