3f4369b0388dd6584224e2b3e8cf5798d693bf6f
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-10-04 18:35:58 -0500

    Eliminé los objetos SQL que se subieron a PRODUCCION en entregas anteriores

--- SQL/Por Entregar/Script de Inicializacion.sql
+++ SQL/Por Entregar/Script de Inicializacion.sql
@@ -10,38 +10,7 @@
 use OrigosVCPalace
 
 -- I. CATALOGOS
---		1. Permisos
---		2. Permisos de personal
 
 -- =============================================
 --					CATALOGOS
 -- =============================================
--- Tabla:		Permisos
--- [wtorres]	07/Sep/2016 Agregue el permiso de exportar reportes a Excel
--- =============================================
-INSERT INTO Permissions (pmID, pmN, pmA, pmToolTipText)
-SELECT 'RPTEXCEL', 'Report Excel', 1, 'Permite exportar los reportes a Excel'
-GO
-
--- =============================================
--- Tabla:		Permisos de personal
--- [wtorres]	07/Sep/2016 Agregue el permiso de exportar reportes a Excel al personal de Contraloria,
---							Soporte Tecnico y Programacion
--- =============================================
-INSERT INTO PersonnelPermissions (pppe, pppm, pppl)
-SELECT peID, 'RPTEXCEL', 1
-FROM Personnel
-WHERE
-	-- CONTR	Contraloria
-	-- SEC		Secretarias De Vents
-	-- SOPORTE	Soporte Tecnico
-	-- PROG		Programacion
-	pede IN ('CONTR', 'SOPORTE', 'PROG', 'SEC')
-	-- Activos
-	AND peA = 1
-	-- 0	No Registrado
-	-- PVP	Usuarios Pvp
-	-- RIFA	Rifa
-	-- TEST	Usuario De Prueba
-	AND peID NOT IN ('0', 'PVP', 'RIFA', 'TEST')
-GO
