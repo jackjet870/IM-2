83dd928c1f1658536fa882cea7cb36f01b452b3d
William Jesus Torres Flota <wtorres@palaceresorts.com>
2016-10-13 10:36:25 -0500

    Deposits. Modifique el campo de numero de tarjeta de credito para que sea varchar(4). Antes era int

--- SQL/Por Entregar/Script de Inicializacion.sql
+++ SQL/Por Entregar/Script de Inicializacion.sql
@@ -10,7 +10,9 @@
 use OrigosVCPalace
 
 -- I. CATALOGOS
---		1.Roles
+--		1. Roles
+--		2. Gifts
+--		3. BookingDeposits
 
 -- =============================================
 --					CATALOGOS
@@ -30,4 +32,12 @@ GO
 -- [emoguel]	10/10/2016 Se Asigno el Amount y Monetary para todos los gifts que contengas el signo de $
 -- =============================================
 UPDATE Gifts SET giMonetary=1,giAmount=SUBSTRING(giN,2,CHARINDEX(' ',giN,1)-2) WHERE giID IN (SELECT giID from Gifts WHERE giN LIKE '$%')
+
+-- =============================================
+-- Tabla:		BookingDeposits
+-- [wtorres]	12/Oct/2016 Modifique el campo de numero de tarjeta de credito para que sea varchar, por eso haya que rellenar de ceros al
+--							principio los numeros de tarjeta que tengan de 1 a 3 caracteres
+-- =============================================
+UPDATE BookingDeposits SET bdCardNum = NULL WHERE bdCardNum = '0'
+UPDATE BookingDeposits SET bdCardNum = dbo.Pad(bdCardNum, '0', 4, 0) WHERE Len(bdCardNum) BETWEEN 1 AND 3
 GO
