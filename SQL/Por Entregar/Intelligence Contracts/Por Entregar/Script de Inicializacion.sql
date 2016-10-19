c7467d912a0840091e78c018aaaa6baa2ff29d40
Edder Salim Rosado Lira <erosado@palaceresorts.com>
2016-10-19 16:37:35 -0500

    Se modifico el tamaño de un control en Sales que no se visualizaba
    Tarea Asignada (Nuevas Posiciones de vendedores)
    Se agregaron los Scrips para IC en la carpeta Por Entregar/Intelligence Contracts/Por Entregar/

---
+++ SQL/Por Entregar/Intelligence Contracts/Por Entregar/Script de Inicializacion.sql
@@ -0,0 +1,64 @@
+/*
+** Palace Resorts
+** Grupo de Desarrollo Palace
+**
+** Script de inicializacion
+** 
+** [wtorres]	20/Feb/2016 Created
+**
+*/
+USE Hotel
+
+-- I. CATALOGOS
+--		1. CLRoles
+--		2. CLCLAOPC
+
+-- =============================================
+--					CATALOGOS
+-- =============================================
+
+-- =============================================
+
+-- Tabla:		analista_h.CLRoles
+-- [erosado]	15/10/2016 Se agregaron dos nuevos roles FTM,FTB
+-- =============================================
+INSERT INTO analista_h.CLRoles ([Id], [Description], [Order], [NumSalesmen])
+SELECT 'FTM','Front To Middle',3,2
+UNION SELECT'FTB','Front To Back',4,2
+GO
+
+-- Se actualizaron el campo Order En Closer, Exit, VLO
+UPDATE Hotel.analista_h.CLRoles
+SET	[Order] =
+	(CASE Id
+		WHEN 'CLOSER' THEN 5
+		WHEN 'EXIT' THEN 6
+		WHEN 'VLO' THEN 7
+	END)
+WHERE Id IN ('CLOSER', 'EXIT', 'VLO')
+GO
+
+-- Se actualizaron el campo NumSales en OPC, Liner, Closer, Exit, Vlo
+UPDATE Hotel.analista_h.CLRoles
+SET	 [NumSalesmen] =
+	(CASE Id
+		WHEN 'OPC' THEN 3
+		WHEN 'LINER' THEN 3
+		WHEN 'CLOSER' THEN 4
+		WHEN 'EXIT' THEN 3
+		WHEN 'VLO' THEN 1
+	END)
+WHERE Id IN ('OPC', 'LINER', 'CLOSER', 'EXIT', 'VLO')
+GO
+
+-- Tabla:		analista_h.CLCLAOPC
+-- [erosado]	17/10/2016 Se actualizaron los valores de Role para FTM y FTB
+-- =============================================
+UPDATE analista_h.CLCLAOPC
+set [Role] = 
+(CASE CLAOPC_ID
+	WHEN 'FTM' THEN 'FTM'
+	WHEN 'FTB' THEN 'FTB'
+	END)
+WHERE CLAOPC_ID IN ('FTM','FTB')
+GO
